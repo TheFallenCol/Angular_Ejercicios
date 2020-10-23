5131655d63692834b24614e528ef799b214f4305
TheFallenCol <germansilva1944@outlook.com>
2020-10-23 17:49:33 -0500

    Singup reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { PanelComponent } from './panel/panel.component';
 import { DirectivasComponent } from './directivas/directivas.component';
@@ -31,7 +31,8 @@ import { InputFormatDirective } from './input-format.directive';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService
--- src/app/singup-form/singup-form.component.ts
+++ src/app/singup-form/singup-form.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'singup-form',
   templateUrl: './singup-form.component.html',
   styleUrls: ['./singup-form.component.scss']
 })
-export class SingupFormComponent implements OnInit {
+export class SingupFormComponent {
+  form = new FormGroup({
+    'user-name': new FormControl('default', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    password: new FormControl('', Validators.required)
+  })
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get username(){
+    return this.form.get("user-name");
   }
 
+  get password(){
+    return this.form.get("password");
+  }
+
+  checkValue(algo){
+    console.log(algo);
+    console.log(this.form.get("user-name"));
+  }
 }
