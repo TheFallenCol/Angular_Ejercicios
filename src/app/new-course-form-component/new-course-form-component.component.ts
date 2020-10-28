cebe13fa4c2decdb7fffeb96e3ee8644a1457cb6
TheFallenCol <germansilva1944@hotmail.com>
2020-10-28 14:02:35 -0500

    Form Builder

--- src/app/new-course-form-component/new-course-form-component.component.ts
+++ src/app/new-course-form-component/new-course-form-component.component.ts
@@ -1,4 +1,4 @@
-import { FormArray, FormGroup, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-course-form-component.component.html',
   styleUrls: ['./new-course-form-component.component.scss']
 })
+
 export class NewCourseFormComponentComponent {
   form = new FormGroup({
     topics : new FormArray([])
   });
+
+  //Ejemplo de como se ve el formbuilder en estructura de form components
+  // form = new FormGroup({
+  //   name : new FormControl('', Validators.required),
+  //   contact : new FormGroup({
+  //     email : new FormControl(),
+  //     phone : new FormControl()
+  //   }),
+  //   topics : new FormArray([])
+  // });
+
+  constructor(fb : FormBuilder){
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact : fb.group({
+        email: [],
+        phone: []
+      }),
+      topics : fb.array([])
+    });
+  }
 
   addTopic(topic : HTMLInputElement){
     this.topics.controls.push(new FormControl(topic.value));
