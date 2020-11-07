a9fae2165debccea900c1caa7ca9cec67d33f0f6
TheFallenCol <germansilva1944@outlook.com>
2020-11-06 22:08:18 -0500

    Combinacion de observadores y switchMap

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,8 @@
 import { GithubFollowerService } from './../services/github-follower.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'm-github-followers',
@@ -9,12 +12,23 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowerService) { }
+  constructor(private route : ActivatedRoute, private service: GithubFollowerService) { }
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe(followers => {this.followers = followers
-        console.log(followers);
-      });
+
+    //this.route.paramMap -> se pone como ejemplo, para este caso no hay una url de la siguiente forma followers/54545?page=1&order=newest
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .pipe(
+      switchMap(combined => {        
+        console.log(combined[0].get('id'));
+        console.log(combined[1].get('page'));
+
+        return this.service.getAll();         
+      })
+    )
+    .subscribe( followers => this.followers = followers );    
   }
 }
