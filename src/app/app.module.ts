ed4eccf7ae6338416e078ec34823b4220e6834a1
TheFallenCol <germansilva1944@outlook.com>
2020-11-06 22:07:00 -0500

    cambio ruteo followers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,7 +59,7 @@ import { NavbarComponent } from './navbar/navbar.component';
         component: HomeComponent 
       },
       {
-        path:'followers/:id', 
+        path:'followers/:id/:username', 
         component: GithubProfileComponent 
       },
       {
