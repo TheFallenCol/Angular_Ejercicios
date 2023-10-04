e6669903efdd1def32afc5dcaabbd694e8cf99d3
German Silva <german.silva@bicode.co>
2023-10-04 12:34:32 -0500

    Add bases project

---
+++ Fernando Course/02-bases/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/components/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Fernando Course/02-bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: '',
+    power: 0
+  }];
+
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter( id?: string): void{
+    if( !id ) return;
+    this.onDeleteId.emit(id);
+  }
+}
